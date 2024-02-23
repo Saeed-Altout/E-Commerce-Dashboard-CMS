97cb332d73ea92191e8201764e765a50ebb19c08
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-02-23 18:50:44 +0300

    create two function getUserby email and id

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,36 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas/login-schema";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
+
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, name, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already has taken!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  // TODO: Send verifcation token email
+
+  return { success: "User Created!" };
 };
