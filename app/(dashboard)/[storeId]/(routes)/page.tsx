ba7d31f6c44b87a742d03d607bc1580fa6f9efa7
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-03-02 01:45:56 +0300

    create dashboard page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,15 @@
-export default function DashboradPage() {
-  return <div>DashboradPage</div>;
+import { db } from "@/lib/db";
+
+interface DashboradPageProps {
+  params: { storeId: string };
+}
+
+export default async function DashboradPage({ params }: DashboradPageProps) {
+  const store = await db.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+
+  return <div>Active store: {store.name}</div>;
 }
